0958f573657628172e665304c7faeb2019af0e38
saebomi <saebomi0324@naver.com>
2020-04-26 15:14:57 +0900

    단일파일 컴포넌트

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<!--template은 html을 기반으로 한 템플릿 언어로 작성-->
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+:root{
+  --sample-weight: 900;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.sample{
+  font-weight:var(--sample-weight);
+}
+</style>
