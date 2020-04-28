6bf6b76127b47c50b370364b2daace3512be7b31
saebomi <saebomi0324@naver.com>
2020-04-28 23:24:56 +0900

    messages 배열추가

--- src/components/Chat/chat.js
+++ src/components/Chat/chat.js
@@ -1,9 +1,16 @@
 export default{
     name:'chat',
+    methods: {
+        send_message(){ // message값을 messages에 추가 후 초기화
+            this.messages.push(this.message)
+            this.message = ""
+        }
+    },
     data(){
         return{
             channels:["general", "random"],
-            message: "" // 새로추가
+            message: "",
+            messages : [] // 폼에서 입력한 데이터를 배열로 저장
         }
     }
 }
